618a8687beadc46d4293f78024a69334e3aa5faf
Vilson <instrutorvilsonmoro@gmail.com>
2022-04-19 11:14:14 -0300

    Introdução ao typescript

---
+++ aula19-04-typescript/funcoes.ts
@@ -0,0 +1,3 @@
+var nome: string;
+nome = 'vilson'
+console.log(nome)
---
+++ aula19-04-typescript/index.ts
@@ -0,0 +1,9 @@
+var nome: string;
+var idade: number;
+var ativo: boolean;
+
+nome = 'Vilson';
+idade = 46;
+ativo = false;
+
+console.log(`Nome: ${nome}  Idade:${idade}  Ativo:${ativo}`)
