fe53b00d1c2f6eff2c2020d83043a4532821fa9c
Vilson <instrutorvilsonmoro@gmail.com>
2022-04-19 11:14:41 -0300

    Tipo array

---
+++ aula19-04-typescript/array.ts
@@ -0,0 +1,8 @@
+var frutas: string[] = ['maça','pera','uva']
+console.log(frutas)
+
+frutas.push('banana')
+console.log(frutas)
+
+var numerosSorteados: number[] = [10,20,30,40,50]
+console.log(numerosSorteados)
