06d07b28e3b168e4ce8d43b0e8a042bc886f6bfc
Vilson <instrutorvilsonmoro@gmail.com>
2022-04-19 11:15:08 -0300

    Type union

---
+++ aula19-04-typescript/union.ts
@@ -0,0 +1,16 @@
+var cpf:(string | number);
+cpf = "123.456.789-89"
+console.log(cpf)
+
+cpf = 12345678989
+console.log(cpf)
+
+var sexo: (string | number | boolean);
+sexo = "masculino"
+console.log(sexo)
+
+sexo = 1
+console.log(sexo)
+
+sexo = false
+console.log(sexo)
