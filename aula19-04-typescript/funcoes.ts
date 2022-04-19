197cf77c3a4d840bc26bbef3c7526a5544c647a3
Vilson <instrutorvilsonmoro@gmail.com>
2022-04-19 14:46:28 -0300

    Criar pagina html para consumir função, validando dados

--- aula19-04-typescript/funcoes.ts
+++ aula19-04-typescript/funcoes.ts
@@ -1,4 +1,13 @@
-function soma(n1: number,n2: number):number{
-    return n1 + n2
+function soma(n1: number,n2: number): (number |  unknown){
+   try{
+      if((n1 == 0) || (n2 == 0)){
+        throw "parâmetros não informado"
+      } 
+      return n1 + n2     
+   }
+   catch(err){
+      console.log("passou aqui")
+      return err
+   } 
+     
 }
-console.log(soma(12, 20))
