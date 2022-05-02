f8108cc0dfec93bc6545a3dc9c783887f92e3e58
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-02 13:03:43 -0300

    adicionar base de dados

---
+++ aula0205/db.js
@@ -0,0 +1,24 @@
+let clientes = [{
+        id: 1,
+        nome: "Ana da Silva",
+        fone: "123-5678",
+        limiteCredito: 1000
+
+    },
+    {
+        id: 2,
+        nome: "JOana da Silva",
+        fone: "123-5678",
+        limiteCredito: 2.500
+
+    },
+    {
+        id: 3,
+        nome: "Pedro da Silva",
+        fone: "123-5562",
+        limiteCredito: 2000
+
+    }
+]
+
+module.exports = clientes
