29eac16c4e05992f5ab4822387bb75f6c34b67e2
Vilson <instrutorvilsonmoro@gmail.com>
2022-05-13 09:09:26 -0300

    Adicionar arquivo de orquestraçaõ para gerar container do app

--- aula1205-docker/index.js
+++ aula1205-docker/index.js
@@ -6,30 +6,32 @@ const HOST = "0.0.0.0"
 const PORT = "8080"
 
 listaClientes = [{
-        nome: 'maria',
-        fone: '123',
-        email: 'maria@gmail.com'
-    },
-    {
-        nome: 'Antonio',
-        fone: '456',
-        email: 'antonio@gmail.com'
-    },
-    {
-        nome: 'Pedro',
-        fone: '789',
-        email: 'pedro@gmail.com'
-    }
-]
-
-const pool = new pg.Pool({
-    user: 'postgres',
-    password: 'admin',
-    database: 'postgres',
-    host: 'meubanco',
-    port: 5432
-})
-
+            nome: 'maria',
+            fone: '123',
+            email: 'maria@gmail.com'
+        },
+        {
+            nome: 'Antonio',
+            fone: '456',
+            email: 'antonio@gmail.com'
+        },
+        {
+            nome: 'Pedro',
+            fone: '789',
+            email: 'pedro@gmail.com'
+        }
+    ]
+    /*
+    const pool = new pg.Pool({
+        user: 'postgres',
+        password: 'admin',
+        database: 'postgres',
+        host: 'meubanco',
+        port: 5432
+    })
+    */
+var consString = "postgres://vaaxwosutwatns:8997e0f6cbef74356d04198c9c52d3bf92611b167a0b8319846bed5dac75e9c3@ec2-107-22-238-112.compute-1.amazonaws.com:5432/d1unpjaoivanab"
+const pool = new pg.Pool({ connectionString: consString, ssl: { rejectUnauthorized: false } })
 app.get('/conectar', (req, res) => {
     pool.connect((err, client) => {
         if (err) {
